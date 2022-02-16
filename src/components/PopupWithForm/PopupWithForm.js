f8dd5c32728039aec45b5ad045bb81f554b572de
e-zybkin <zybkin.evgeniy@gmail.com>
2022-02-16 10:30:43 +0300

    feat: universal popup block

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -1,24 +1,19 @@
-import React from "react";
+import Popup from "../Popup/Popup";
 
-function PopupWithForm(props) {
-  const popupClass = `popup popup_type_others popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`;
-
-  return(
-    <div className={popupClass}>
-			<div className={`popup__content popup__${props.name}-content`}>
-				<button type="button" onClick={props.onClose} className="popup__close-btn buttons"></button>
-				<h3 className="popup__title">{props.title}</h3>
-				<form
-          className={`popup__form popup__form_type_${props.name}`}
-          name={props.name}
-          onSubmit={props.onSubmit}
-        >
-          {props.children}
-          <button type="submit" className="popup__save-btn">{props.submitText}</button>
-				</form>
-			</div>
-		</div>
-  );
+function PopupWithForm({isOpen, name, onClose, ...props}) {
+  return (
+    <Popup isOpen={isOpen} name={name} onClose={onClose} content={true}>
+      <h3 className='popup__title'>{props.title}</h3>
+      <form
+        className={`popup__form popup__form_type_${props.name}`}
+        name={props.name}
+        onSubmit={props.onSubmit}
+      >
+        {props.children}
+        <button type="submit" className="popup__save-btn">{props.submitText}</button>
+			</form>
+    </Popup>
+  )
 }
 
 export default PopupWithForm;
